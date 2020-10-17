5b73d2977ea320e8223bd100281bb1cd1b703331
Hoghe <72462696+Hoghe@users.noreply.github.com>
2020-10-17 15:41:00 +0200

    Version 1.0.0

---
+++ Test_Maik/src/MainMenuBar.java
@@ -0,0 +1,48 @@
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class MainMenuBar extends MenuBar implements ActionListener{
+	
+	private Menu _menu;
+	private MenuItem _color1, _color2, _color3;
+	
+	public MainMenuBar() {
+		_menu = new Menu("Ändere die Hintergrundfarbe");
+				
+//		_menu.add(new MenuItem("color: red"));
+//		_menu.add(new MenuItem("color: yellow"));
+//		_menu.add(new MenuItem("color: green"));
+		
+		_color1 = new MenuItem("Farbe: Rot");
+		_color1.addActionListener(this);
+		_menu.add(_color1);
+		_color2 = new MenuItem("Farbe: Gelb");
+		_color2.addActionListener(this);
+		_menu.add(_color2);
+		_color3 = new MenuItem("Farbe: Blau");
+		_color3.addActionListener(this);
+		_menu.add(_color3);
+			
+		add(_menu);
+		}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		if (e.getSource() == _color1) {
+			System.out.println("test rot");
+			setBackground(Color.red);
+		}else if (e.getSource() == _color2) {
+			System.out.println("test gelb");
+			setBackground(Color.green);
+		}else if (e.getSource() == _color3) {
+			System.out.println("test blau");
+			setBackground(Color.blue);
+		}
+	}
+
+}
+
---
+++ Test_Maik/src/MainWindow.java
@@ -0,0 +1,79 @@
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class MainWindow extends Frame implements WindowListener {
+	
+	private int _x, _y;
+
+	
+	public MainWindow(int windowSizeX, int windowSizeY) {
+		this._x = windowSizeX;
+		this._y = windowSizeY;
+		setTitle("Programm für Softwaretechnik");
+		setSize(_x,_y);
+		addWindowListener(this);
+	//	add(new DrawObject(_x, _y));
+		DrawObject d = new DrawObject(_x, _y);
+		add(d);
+		setMenuBar(new MainMenuBar());
+
+		
+	}
+	
+	public int getX () {
+		return _x;
+	}
+	
+	public int getY () {
+		return _y;
+	}
+	
+	public void openWindow() {
+		setVisible(true);
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		dispose();	//	Ressourcenfreigabe
+		System.exit(0);
+
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Automatisch generierter Methodenstub
+		
+	}
+}
